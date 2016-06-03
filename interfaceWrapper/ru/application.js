471407e2dc53548369cfcb610e0eb04308ecf870
0men <sensei515@mail.ru>
2016-06-03 15:03:16 +0300

    interface wrapper: task 6

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,7 +1,21 @@
-console.log('From application global context');
+"use strict";
+
+console.log('From application!\n');
 
 var fileName = './README.md';
 console.log('Application going to read ' + fileName);
 fs.readFile(fileName, function(err, src) {
 	console.log('File ' + fileName + ' size ' + src.length);
+});
+
+fs.writeFile('text.txt', 'Hello World', function (err) {
+    if (err) {
+        return console.log(err);
+    }
+});
+
+fs.readFile('text.txt', function(err) {
+    if (err) {
+        return console.log(err);
+    }
 });
--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -1,42 +1,50 @@
-// Пример оборачивания функции в песочнице
-
+'use strict';
 var fs = require('fs'),
-vm = require('vm');
+    vm = require('vm');
 
 function cloneInterface(myInterface) {
-  function clone(obj, res) {
-    Object.keys(myInterface).forEach((key) => {
-      if (typeof obj === 'Object') {
-       res[key] = {};
-       clone(obj, res[key]);
-      } else {
-        res[key] = obj[key];
-      }
-    });
-  }
- 
-  const interfaceClone = {};
-  clone(myInterface, interfaceClone);
- 
-  return interfaceClone;
+    function clone(obj, res) {
+        for (var key in obj) {
+            if (typeof obj[key] === 'object') {
+                res[key] = {};
+                clone(obj[key], res[key]);
+            } else {
+                res[key] = wrapFunction(key, obj[key]);
+            }
+        }
+    }
+
+    const interfaceClone = {};
+    clone(myInterface, interfaceClone);
+    return interfaceClone;
 }
 
 function wrapFunction(fnName, fn) {
-  return function wrapper() {
-    var args = [];
-    Array.prototype.push.apply(args, arguments);
-    console.log('Call: ' + fnName);
-    console.dir(args);
-    return fn.apply(undefined, args);
-  }
+    fs.writeFileSync('file.log', '');
+    return function wrapper() {
+        var args = [];
+        Array.prototype.push.apply(args, arguments);//args.push(arguments);
+        console.log('Call function: ' + fnName + '()');
+        //console.dir(args);
+        if (typeof args[args.length - 1] === 'function') {
+            var callBack = args[args.length - 1];
+            args[args.length - 1] = function () {
+                var cbArgs = [];
+                Array.prototype.push.apply(cbArgs, arguments);
+                fs.appendFileSync('file.log', 'Call function: ' + fnName + '()\n' + cbArgs.toString() + '\n');
+                return callBack.apply(undefined, cbArgs);
+            };
+        }
+        return fn.apply(undefined, args);
+    }
 }
 
 // Объявляем хеш из которого сделаем контекст-песочницу
 var context = {
-  module: {},
-  console: console,
-  // Помещаем ссылку на fs API в песочницу
-  fs: cloneInterface(fs)
+    module: {},
+    console: console,
+    // Помещаем ссылку на fs API в песочницу
+    fs: cloneInterface(fs)
 };
 
 // Преобразовываем хеш в контекст
@@ -45,8 +53,8 @@ var sandbox = vm.createContext(context);
 
 // Читаем исходный код приложения из файла
 var fileName = './application.js';
-fs.readFile(fileName, function(err, src) {
-  // Запускаем код приложения в песочнице
-  var script = vm.createScript(src, fileName);
-  script.runInNewContext(sandbox);
+fs.readFile(fileName, function (err, src) {
+    // Запускаем код приложения в песочнице
+    var script = vm.createScript(src, fileName);
+    script.runInNewContext(sandbox);
 });
